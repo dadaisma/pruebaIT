72a191631142bf78c41dadb6cdbff5f0e0adf3f8
dadaisma <infodadaisma@gmail.com>
2024-05-07 12:44:02 +0200

    orderstructure

--- src/order/OrderStructure.java
+++ src/order/OrderStructure.java
@@ -1,7 +1,11 @@
 package order;
 
+import exceptions.*;
+import menu.UI;
 import people.Customer;
 import people.Rider;
+import products.*;
+import speed.Speed;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +34,144 @@ public class OrderStructure {
 
     //order create
 
-    public static void newOrder()
+    public static void newOrder()throws CustomerUndefinedException, EmptyProductListException, IDundefined, InvalidMenuOptionException, NoAvailableRider{
+        Rider rider;
+        Customer customer;
+        List<Products> products;
+        Speed speed;
+
+        customer = pickCustomer();
+        products = pickProductList();
+        rider = pickRider();
+        speed = pickSpeed();
+
+        Order order = new Order(customer, rider, products,speed);
+        orders.add(order);
+        System.out.println("Order completed!");
+    }
+
+        private static Customer pickCustomer()throws CustomerUndefinedException{
+        String name;
+            UI.customerMenu();
+            name=sc.nextLine();
+            for (Customer customer : customers){
+                if(customer.getName().equals(name)){
+                    return customer;
+                }
+            }
+            throw new CustomerUndefinedException();
+        }
+
+        private static Rider pickRider()throws  NoAvailableRider{
+        if(riders.stream().allMatch(rider -> !rider.isAvailable())){
+            throw new NoAvailableRider();
+        }
+        int randomIndex = random.nextInt(riders.size());
+        boolean free = false;
+
+        do{
+            if(riders.get(randomIndex).isAvailable()){
+                free = true;
+                riders.get(randomIndex).setAvailable(false);
+                return riders.get(randomIndex);
+            }
+            randomIndex = random.nextInt(riders.size());
+        } while(!free);
+        return null;
+
+        }
+
+        //speed
+    private static Speed pickSpeed() throws  InvalidMenuOptionException{
+        UI.speedMenu();
+        option = sc.nextLine();
+        switch (option){
+            case "2":
+                return new Speed(false,true,false);
+            case "3":
+                return new Speed(false,false,true);
+            case "1":
+                return new Speed(true,false,false);
+            default:
+                throw new InvalidMenuOptionException();
+        }
+    }
+
+    private static List<Products> pickProductList()throws InvalidMenuOptionException, EmptyProductListException{
+        List<Products> products = new ArrayList<Products>();
+
+        do{
+            UI.productsListMenu();
+            option = sc.nextLine();
+            switch (option){
+                case "1":
+                    products.add(pickProduct());
+                    option="";
+                    break;
+                case "2":
+                    break;
+                default:
+                    throw new InvalidMenuOptionException();
+            }
+             } while (!option.equals("2"));
+            if(products.isEmpty()){
+                throw new EmptyProductListException();
+            }
+            return products;
+    }
+
+    private static Products pickProduct() throws  InvalidMenuOptionException{
+        UI.productsMenu();
+        option = sc.nextLine();
+        switch (option){
+            case "1":
+                return new Burritos();
+            case "2":
+                return new Hamburger();
+            case "3":
+                return new Kebab();
+            case "4":
+                return new Pizza();
+            default:
+                throw new InvalidMenuOptionException();
+        }
+    }
+
+    // Delivery
+
+    public static void deliveredOrder() throws  NumberFormatException, IDundefined, NullPointerException{
+        int id;
+        Order o;
+
+        UI.deliveryMenu();
+        id= Integer.parseInt(sc.nextLine());
+
+        o= orders.stream()
+                .filter(order -> order.getID()== id)
+                .findFirst()
+                .orElse(null);
+
+        if(o==null) {
+            throw new IDundefined();
+        }
+        o.getRider().setAvailable(true);
+        orders.remove(o);
+        deliveredOrders.add(o);
+        System.out.println("Order successfully Delivered!");
+    }
+
+    // order List (Pending)
+
+    public static void listOrders() {
+        System.out.println("PENDING ORDERS");
+        orders.forEach(order -> System.out.println(order));
+    }
+
+    // delivered orders
+
+    public static void listDeliveredOrders(){
+        System.out.println("ORDERS DELIVERED");
+        deliveredOrders.forEach(order -> System.out.println(order));
+    }
 
 }
